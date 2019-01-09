70a4df0b757bf33ee2be5a955df07c06ea14dce2
Fatih Çakmak <cakmakfatih@outlook.com.tr>
2019-01-09 12:49:32 +0300

    before deploy

--- src/layouts/Editor.tsx
+++ src/layouts/Editor.tsx
@@ -4,15 +4,17 @@ import Builder from './../builder/Builder';
 import Layout from './../components/Layout';
 import { Link } from 'react-router-dom';
 
-export default class Editor extends React.Component<{}, { 
-    builder: Builder,
-    venueOutline: string
-    }> {
+export default class Editor extends React.Component {
     
+    builder: Builder;
+    venueOutline: string;
+
     constructor(props: any) {
         super(props);
+
+        this.changeValues = this.changeValues.bind(this);
+
         this.state = {
-            builder: null,
             venueOutline: ""
         };
     }
@@ -22,25 +24,12 @@ export default class Editor extends React.Component<{}, {
         
         let b: Builder = new Builder(data, this.refs["3d-view-container"] as HTMLDivElement);
 
-        this.setState({
-            builder: b
-        });
+        this.builder = b;
     }
 
-    flipHexString(hexValue: string, hexDigits: number) {
-        var h = hexValue.substr(0, 2);
-        for (var i = 0; i < hexDigits; ++i) {
-          h += hexValue.substr(2 + (hexDigits - 1 - i) * 2, 2);
-        }
-        return h;
-      }
-      
-      
-      hexToFloat(hex: number): number {
-        var s = hex >> 31 ? -1 : 1;
-        var e = (hex >> 23) & 0xFF;
-        return s * (hex & 0x7fffff | 0x800000) * 1.0 / Math.pow(2, 23) * Math.pow(2, (e - 127))
-      }
+    changeValues() {
+        this.builder.setVenueColor(parseInt(this.venueOutline, 16));
+    }
     
     render() {
         return (
@@ -50,10 +39,10 @@ export default class Editor extends React.Component<{}, {
                         <Link to='/'><i className="fas fa-chevron-left"></i></Link>
                         <div className="form-group">
                             <label htmlFor="" className="label-default">Venue Outline</label>
-                            <input type="text" maxLength={6} placeholder="Hex" className="inp-default" />
+                            <input type="text" maxLength={6} placeholder="Hex" className="inp-default" onChange={(e) => this.venueOutline = e.target.value} />
                         </div>
                     </section>
-                    <button className="btn-default" onClick={() => this.state.builder.setVenueColor(0xff2020)}>Save</button>
+                    <button className="btn-default" onClick={this.changeValues}>Save</button>
                 </aside>
                 <div ref="3d-view-container" id="geo3d-view-container" />
             </Layout>
