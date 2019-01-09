416b7b7bcef238b81bc2ad68d1630da123110bcf
Fatih Çakmak <cakmakfatih@outlook.com.tr>
2019-01-10 01:33:45 +0300

    map rotated

--- src/builder/Builder.ts
+++ src/builder/Builder.ts
@@ -15,29 +15,24 @@ class Builder {
 
     constructor(data: any, container: HTMLDivElement) {
         this.data = data;
-        this.venueColor = 0x000000;
+        this.venueColor = 0x00ff00;
 
         this.render = this.render.bind(this);
         this.update = this.update.bind(this);
         this.viewLoop = this.viewLoop.bind(this);
         
         this.scene = new THREE.Scene();
-        this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100000);
+        this.camera = new THREE.PerspectiveCamera(75, container.getBoundingClientRect().width / container.getBoundingClientRect().height, 0.1, 100000);
         this.renderer = new THREE.WebGLRenderer({ antialias: true });
         this.renderer.shadowMap.enabled = true;
-        this.renderer.setSize(container.getBoundingClientRect().width, window.innerHeight);
+        this.renderer.setSize(container.getBoundingClientRect().width, container.getBoundingClientRect().height);
         container.appendChild(this.renderer.domElement);
 
         window.addEventListener('resize', () => {
-            this.renderer.setSize(container.getBoundingClientRect().width, window.innerHeight);
-            this.camera.aspect = container.offsetWidth / window.innerHeight;
+            this.renderer.setSize(container.getBoundingClientRect().width, container.getBoundingClientRect().height);
+            this.camera.aspect = container.offsetWidth / container.getBoundingClientRect().height;
             this.camera.updateProjectionMatrix();
         });
-
-        this.camera.position.z = 50;
-        this.camera.position.y = 50;
-        this.camera.position.x = 100;
-        this.camera.lookAt(30, 40, 60);
 
         this.controls = new OrbitControls(this.camera, this.renderer.domElement);
         
@@ -47,15 +42,17 @@ class Builder {
 
     addSkybox() {
         // to-do add skybox
-        let geometry = new THREE.BoxGeometry(5000, 5000, 5000);
+        let geometry = new THREE.BoxGeometry(10000, 10000, 10000);
 
         let textureLoader = new THREE.TextureLoader();
 
-        // https://api.mapbox.com/styles/v1/mapbox/light-v9/static/29.07107,41.00360,15.0,0,0/1280x1280?access_token=pk.eyJ1IjoiY2FrbWFrZmF0aWgiLCJhIjoiY2pxcGk1d3ZrMDFwYjQ5bzFqNncyYjl2NyJ9.MtGJZ74Cu-6R7K52rFrNeQ
+        let mapUrl = "https://api.mapbox.com/styles/v1/mapbox/light-v9/static/29.07107,41.00360,15.0,0,0/1280x1280?access_token=pk.eyJ1IjoiY2FrbWFrZmF0aWgiLCJhIjoiY2pxcGk1d3ZrMDFwYjQ5bzFqNncyYjl2NyJ9.MtGJZ74Cu-6R7K52rFrNeQ"
 
         let map = textureLoader.load("staticmap.png");
-
-        map.minFilter = THREE.LinearFilter;
+        map.anisotropy = this.renderer.capabilities.getMaxAnisotropy();
+        map.minFilter = THREE.NearestFilter;
+        map.wrapS = THREE.RepeatWrapping;
+        map.repeat.x = - 1;
 
         let mat = new THREE.MeshBasicMaterial({
             side: THREE.DoubleSide,
@@ -63,7 +60,7 @@ class Builder {
         });
 
         let cube = new THREE.Mesh(geometry, mat);
-        cube.position.setY(2500);
+        cube.position.setY(5000);
 
         this.scene.add(cube);
     }
@@ -86,6 +83,8 @@ class Builder {
 
     setOffsets(coords: number[]) {
         this.vectorGenerator = new VectorGenerator(undefined, coords[0], coords[1]);
+        this.camera.position.set(700, 700, -700);
+        this.camera.lookAt(0, 0, 0);
     }
 
     setVenueColor(color: number){
@@ -132,7 +131,7 @@ class Builder {
     }
 
     addMultiPolygon(i: any) {
-       let material = new THREE.LineBasicMaterial({
+        let material = new THREE.LineBasicMaterial({
             color: this.venueColor
         });
         
@@ -142,8 +141,7 @@ class Builder {
             j.forEach((k: any) => {
                 k.forEach((q: any) => {
                     let scaledVector: ScaledVector = this.vectorGenerator.generateVector(q);
-
-                    let vector: Vector3 = new THREE.Vector3(scaledVector.x, scaledVector.y, scaledVector.z);
+                    let vector: Vector3 = new THREE.Vector3(-scaledVector.x, scaledVector.y, scaledVector.z);
 
                     geometry.vertices.push(vector);
                 })
